51a3b26df675d64459271bbad9575e64d96b18e3
IvanTetsuo <ivan.d2413@gmail.com>
2024-10-01 21:41:38 +0300

    вынес данные о подключнии в ЕНВ

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,11 +28,11 @@ import { Task } from './entities/task.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'notasecret',
-      database: 'todo_list',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [
         User,
         Project,
